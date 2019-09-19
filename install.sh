099548d958e68c3d51f8c737a2dbce984f979865
Magnus Wallin <magnuswallin@zoho.com>
2019-09-19 14:21:40 +0200

    Revert log dir, simplify service
    We don't need a separate log file as we log to syslog.
    service file simplified, no need for:
    Restart=OnFailure

--- install.sh
+++ install.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-logdir="/var/log/batmon/"
-logfile="batmon.log"
-
-
 if (( $EUID != 0 )); then
     echo "You need to run as root. Exiting."
     exit 1
-fi
-
-# Create log dir if needed
-if [[ ! -d "$logdir" ]]; then
-    echo "Creating log directory $logdir"
-    mkdir "$logdir" && touch "$logdir$logfile"
 fi
 
 # Copy binary to /opt
@@ -23,5 +13,7 @@ cp -v systemd/batmon.{service,timer} /etc/systemd/system/
 
 # Start and enable timer
 systemctl start batmon.timer && systemctl enable batmon.timer
+echo "Running \"systemctl daemon-reload\""
+systemctl daemon-reload
 
 exit 0
