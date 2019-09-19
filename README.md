1f9de8c63e20260140e1cb679ef73f0dba05bdb7
Magnus Wallin <magnuswallin@zoho.com>
2019-09-19 14:31:00 +0200

    More verbosity in install script
    ...also updated README

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # batmon
 Power off server on low battery
+
+### Install script
+```
+sudo ./install.sh 
+Installing binary in /opt/batmon
+'binary/batmon' -> '/opt/batmon'
+Installing systemd files
+'systemd/batmon.service' -> '/etc/systemd/system/batmon.service'
+'systemd/batmon.timer' -> '/etc/systemd/system/batmon.timer'
+Warning: The unit file, source configuration file or drop-ins of batmon.timer changed on disk. Run 'systemctl daemon-reload' to reload units.
+Running "systemctl daemon-reload"
+
+NOTE:
+batmon logs to syslog. Run:
+sudo journalctl -u batmon
+to see log entries.
+```
+
--- install.sh
+++ install.sh
@@ -6,14 +6,21 @@ if (( $EUID != 0 )); then
 fi
 
 # Copy binary to /opt
+echo "Installing binary in /opt/batmon"
 cp -v binary/batmon /opt/
 
 # Place systemd service & timer
+echo "Installing systemd files"
 cp -v systemd/batmon.{service,timer} /etc/systemd/system/
 
 # Start and enable timer
 systemctl start batmon.timer && systemctl enable batmon.timer
-echo "Running \"systemctl daemon-reload\""
+echo -e "Running \"systemctl daemon-reload\"\n"
 systemctl daemon-reload
+
+echo "NOTE:"
+echo "batmon logs to syslog. Run:"
+echo "sudo journalctl -u batmon"
+echo "to see log entries."
 
 exit 0
